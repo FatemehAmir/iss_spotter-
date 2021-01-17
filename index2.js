bf6511129f909292c0506fecd175250538cee3b4
FatemehAmir <f.amirkhanloo64@gmai.com>
2021-01-17 01:31:49 +0000

    new

---
+++ index2.js
@@ -0,0 +1,13 @@
+const { nextISSTimesForMyLocation } = require('./iss_promised');
+
+
+fetchMyIP()
+  .then(fetchCoordsByIP)
+  .then(fetchISSFlyOverTimes)
+  .then(body => console.log(body));
+
+
+nextISSTimesForMyLocation()
+  .then((passTimes) => {
+    printPassTimes(passTimes);
+  })
